c3c5d5b28a9c4ddc29328af664fff7192710336b
agoworek <amadeo.go@vp.pl>
2022-03-18 17:37:20 +0100

    Add Patient

--- VaccinationSystem/Models/Patient.cs
+++ VaccinationSystem/Models/Patient.cs
@@ -5,7 +5,13 @@ using System.Threading.Tasks;
 
 namespace VaccinationSystem.Models
 {
-    public class Patient
+    public class Patient : User
     {
+        //public Dictionary<Virus,int> VaccinationsCount { get; set; }
+        //public List<Appointment> VaccinationHistory { get; set; }
+        //public List<Appointment> FutureVaccinations { get; set; }
+        //List<Certificate> Certificates { get; set};
+        bool Active;
+
     }
 }
--- VaccinationSystem/Models/User.cs
+++ VaccinationSystem/Models/User.cs
@@ -5,7 +5,15 @@ using System.Threading.Tasks;
 
 namespace VaccinationSystem.Models
 {
-    public class User
+    public abstract class User
     {
+        public string Id { get; set; }
+        public string PESEL { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        DateTime DateOfBirth { get; set; }
+        string Mail { get; set; }
+        string Password { get; set; }
+        string PhoneNumber { get; set; }
     }
 }
