0ebcf10bd29f2e4a8cfa9c4c0153293c7ceffda4
agoworek <amadeo.go@vp.pl>
2022-03-19 11:54:42 +0100

    Add DefaultController Scaffold

---
+++ VaccinationSystem/Controllers/DefaultController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VaccinationSystem.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VaccinationSystem.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+
+        [Route("/register")]
+        [HttpPost]
+        public IActionResult Register([FromBody] Patient patient)
+        {
+            return NotFound("Not implemented yet");
+        }
+        [Route("/Signin")]
+        [HttpPost]
+        public IActionResult SignIn([FromBody] Login login)
+        {
+            return NotFound("Not implemented yet");
+
+        }
+        [Route("/user/Logout/{userId}")]
+        [HttpPost]
+        public IActionResult Post([FromRoute] string userId)
+        {
+            return NotFound("Not implemented yet");
+
+        }
+
+    }
+}
---
+++ VaccinationSystem/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VaccinationSystem.Models
+{
+    public class Login
+    {
+        public string mail { get; set; }
+        public string password { get; set; }
+    }
+}
