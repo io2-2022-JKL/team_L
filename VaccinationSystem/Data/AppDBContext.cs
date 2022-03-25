945264b54999ab3e6f0754a70574a2bf96bfe522
Evva28 <93273156+Evva28@users.noreply.github.com>
2022-03-25 16:55:10 +0100

    dodany connection string oraz db context

---
+++ VaccinationSystem/Data/AppDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VaccinationSystem.Models;
+
+namespace VaccinationSystem.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
