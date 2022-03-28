e098a011450fa8054516bc058437ddcfab63c4b2
AliaksandrBialchytski <73606879+AliaksandrBialchytski@users.noreply.github.com>
2022-03-28 11:14:26 +0200

    Add DoctorsController

---
+++ VaccinationSystem/Contollers/DoctorsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VaccinationSystem.Data;
+using VaccinationSystem.Models;
+
+namespace VaccinationSystem.Contollers
+{
+    public class DoctorsController : Controller
+    {
+        private readonly AppDBContext _context;
+
+        public DoctorsController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        [Route("/admin/doctors/showDoctors")]
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<Doctor>>> GetDoctors(Doctor doctor)
+        {
+            var doctors = _context.Doctors.Where(d => d.pesel == doctor.pesel &&
+                                                d.firstName == doctor.firstName &&
+                                                d.lastName == doctor.lastName &&
+                                                d.mail == doctor.mail &&
+                                                d.dateOfBirth == doctor.dateOfBirth &&
+                                                d.phoneNumber == doctor.phoneNumber &&
+                                                d.vaccinationCenter == doctor.vaccinationCenter
+                ).ToListAsync();
+            return await doctors;
+        }
+    }
+}
